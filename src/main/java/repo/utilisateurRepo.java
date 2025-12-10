7ef48f203d94595a5c9ac9d7bdbd35e7559e3cab
kaiyangzhang <18201122059zky@gmail.com>
2025-12-10 11:13:26 +0100

    UtilisateurRepo

---
+++ src/main/java/repo/utilisateurRepo.java
@@ -0,0 +1,55 @@
+package repo;
+
+import bd.HibernateUtil;
+import model.Utilisateur;
+import org.hibernate.Session;
+
+public class utilisateurRepo {
+    public Utilisateur loadUtilisateur(int id) {
+        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            Utilisateur u =  (Utilisateur) session.get(Utilisateur.class, id);
+            session.close();
+            return u;
+        }
+    }
+
+    public void saveUtilisateur(Utilisateur u) {
+        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.save(u);
+            session.getTransaction().commit();
+            session.close();
+        }
+    }
+
+    public Utilisateur updateUtilisateur(Utilisateur u) {
+        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.update(u);
+            session.getTransaction().commit();
+            session.close();
+            return u;
+        }
+    }
+
+    public Utilisateur deleteUtilisateur(Utilisateur u) {
+        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.delete(u);
+            session.getTransaction().commit();
+            session.close();
+            return u;
+        }
+    }
+
+    public Utilisateur refreshUtilisateur(Utilisateur u) {
+        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            long id = u.getIdUtilisateur();
+            Utilisateur u2 = (Utilisateur) session.get(Utilisateur.class, id);
+            session.close();
+            return u2;
+        }
+    }
+}

e95532f9d86a0e02f941e2a5781b13efa9d825cc
kaiyangzhang <18201122059zky@gmail.com>
2025-12-11 18:08:36 +0100

    reset Password

--- src/main/java/util/emailUtil.java
+++ src/main/java/util/emailUtil.java
@@ -10,4 +10,14 @@ public class emailUtil {
         String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
         return email.matches(regex);
     }
+
+    public static String cutEmail(String email) {
+        if (email == null) {
+            return null;
+        }
+        if (email.length() <= 6){
+            return email;
+        }
+        return email.substring(0, 6);
+    }
 }
